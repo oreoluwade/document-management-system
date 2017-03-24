d60f8b75bc1bdb02d25d9e6720c035b4033b4036
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-03-24 08:59:59 +0100

    feature(server): restructure app and server files

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
-// The application entry point
-import http from 'http';
-import app from './app'; // The express app
+/* eslint no-console: "off" */
+
+import express from 'express';
+import parser from 'body-parser';
+import homeRoute from './server/routes/index';
+import documentRoutes from './server/routes/document';
+import userRoutes from './server/routes/user';
+import roleRoutes from './server/routes/role';
+
+require('dotenv').config();
 
 const port = parseInt(process.env.PORT, 10) || 9000;
-app.set('port', port);
 
-const server = http.createServer(app);
-server.listen(port);
-console.log(`server running on port ${port}`);
+// Set up the express app
+const app = express();
 
+// Parse incoming requests data
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+// Specifying the routes the app should use for different purposes
+app.use('/', homeRoute);
+app.use('/role', roleRoutes);
+app.use('/document', documentRoutes);
+app.use('/user', userRoutes);
+
+
+// Start Server
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
+
+module.exports = app;
