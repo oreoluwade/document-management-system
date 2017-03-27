1674737acd3fd215b5ceace3711d8ee567d5b48a
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-03-27 07:46:00 +0100

    feature(pages): create basic templates for home and about pages

---
+++ client/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <p>This application enables a user to create, edit, and delete documents easily</p>
+        <p>It also helps in storing sensitive info, keeping them from the eyes of peeping toms</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
