8796e23a2caab587d485372b42423bdccdb4f667
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-03-28 10:44:35 +0100

    feature(header): modify header to reflect signup

--- client/components/common/Header.js
+++ client/components/common/Header.js
@@ -1,14 +1,14 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 
-const Header = () => {
-  return (
+const Header = () => (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {' | '}
       <Link to="/about" activeClassName="active">About</Link>
+      {' | '}
+      <Link to="/signup" activeClassName="active">SignUp</Link>
     </nav>
   );
-};
 
 export default Header;
