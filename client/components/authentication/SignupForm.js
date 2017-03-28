053314ad5a135de863d8dce04022f2f15a94248a
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-03-28 10:43:54 +0100

    feature(user signup): create signup form, page and textfields template

---
+++ client/components/authentication/SignupForm.js
@@ -0,0 +1,133 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import React from 'react';
+import validateInput from '../../../server/shared/validations/signup';
+import TextFieldGroup from '../common/TextFieldGroup';
+
+class SignupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: '',
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      passwordConfirmation: '',
+      roleId: 2,
+      errors: {},
+      isLoading: false
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  isValid() {
+    const { errors, isValid } = validateInput(this.state);
+
+    if (!isValid) {
+      this.setState({ errors });
+    }
+
+    return isValid;
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    if (this.isValid()) {
+      this.setState({ errors: {}, isLoading: true });
+      this.props.userSignupRequest(this.state).then(
+        () => {
+          this.props.addFlashMessage({
+            type: 'success',
+            text: 'Welcome! You  have signed up successfully'
+          });
+          this.context.router.push('/');
+        },
+        ({ data }) => this.setState({ errors: data, isLoading: false })
+      );
+    }
+  }
+
+  render() {
+    const { errors } = this.state;
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h1><strong>Sign up here</strong></h1>
+
+        <TextFieldGroup
+          error={errors.userName}
+          label="Username"
+          onChange={this.onChange}
+          value={this.state.userName}
+          field="userName"
+        />
+
+        <TextFieldGroup
+          error={errors.firstName}
+          label="First Name"
+          onChange={this.onChange}
+          value={this.state.firstName}
+          field="firstName"
+        />
+
+        <TextFieldGroup
+          error={errors.lastName}
+          label="Last Name"
+          onChange={this.onChange}
+          value={this.state.lastName}
+          field="lastName"
+        />
+
+        <TextFieldGroup
+          error={errors.email}
+          label="Email"
+          onChange={this.onChange}
+          value={this.state.email}
+          field="email"
+        />
+
+        <TextFieldGroup
+          error={errors.password}
+          label="Password"
+          onChange={this.onChange}
+          value={this.state.password}
+          field="password"
+          type="password"
+        />
+
+        <TextFieldGroup
+          error={errors.passwordConfirmation}
+          label="Password Confirmation"
+          onChange={this.onChange}
+          value={this.state.passwordConfirmation}
+          field="passwordConfirmation"
+          type="password"
+        />
+
+        <div className="form-group">
+          <button disabled={this.state.isLoading} className="btn btn-primary btn-lg">
+            Sign up
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+SignupForm.propTypes = {
+  userSignupRequest: React.PropTypes.func.isRequired,
+  addFlashMessage: React.PropTypes.func.isRequired
+};
+
+SignupForm.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
+
+export default SignupForm;
---
+++ client/components/authentication/SignupPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import SignupForm from './SignupForm';
+import { userSignupRequest } from '../../actions/signupActions';
+import { addFlashMessage } from '../../actions/flashMessages';
+
+class SignupPage extends React.Component {
+  render() {
+    const { userSignupRequest, addFlashMessage } = this.props;
+    return (
+      <div>
+        <div className="row">
+          <div className="col-md-4 col-md-offset-4">
+            <SignupForm userSignupRequest={userSignupRequest} addFlashMessage={addFlashMessage} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+SignupPage.propTypes = {
+  userSignupRequest: React.PropTypes.func.isRequired,
+  addFlashMessage: React.PropTypes.func.isRequired
+};
+
+export default connect(null, { userSignupRequest, addFlashMessage })(SignupPage);
