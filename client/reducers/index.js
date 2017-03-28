12d60ea0adad5fcdfdc5322ad9e0521320f94b29
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-03-28 10:39:36 +0100

    feature(reducer): create index file for combining reducers

---
+++ client/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import flashMessages from './flashMessages';
+
+export const rootReducer = combineReducers({
+  flashMessages
+});
+
+export default rootReducer;
