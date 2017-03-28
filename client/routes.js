94521f99c878309ee99847a62a03ac3ae7184395
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-03-28 10:49:42 +0100

    feature(signup): refactor routes to account for signup

--- client/routes.js
+++ client/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
+import SignupPage from './components/authentication/SignupPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="about" component={AboutPage} />
+    <Route path="signup" component={SignupPage} />
   </Route>
 );
