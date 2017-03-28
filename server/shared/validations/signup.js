d93cb8d107d994f84c27d778624ef27a7ce18095
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-03-28 10:37:02 +0100

    feature(validation): create client side validation

---
+++ server/shared/validations/signup.js
@@ -0,0 +1,36 @@
+import Validator from 'validator';
+import isEmpty from 'lodash/isEmpty';
+
+export default function validateInput(data) {
+  const errors = {};
+
+  if (Validator.isEmpty(data.userName)) {
+    errors.userName = 'The field is required';
+  }
+  if (Validator.isEmpty(data.firstName)) {
+    errors.firstName = 'This field is required';
+  }
+  if (Validator.isEmpty(data.lastName)) {
+    errors.lastName = 'This field is required';
+  }
+  if (Validator.isEmpty(data.email)) {
+    errors.email = 'email is invalid';
+  }
+  if (Validator.isEmail(data.email)) {
+    errors.email = 'email is invalid';
+  }
+  if (Validator.isEmpty(data.password)) {
+    errors.password = 'This field is required';
+  }
+  if (Validator.isEmpty(data.passwordConfirmation)) {
+    errors.passwordConfirmation = 'Passwords must match';
+  }
+  if (!Validator.equals(data.password, data.passwordConfirmation)) {
+    errors.passwordConfirmation = 'Passwords must match';
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+}
