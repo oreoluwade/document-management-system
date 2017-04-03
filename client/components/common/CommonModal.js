ea96b6f2022f85f53dbc08a854b04212c3906572
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-04-04 00:24:00 +0100

    feature(document): add document form and modal to display document

---
+++ client/components/common/CommonModal.js
@@ -0,0 +1,47 @@
+/* eslint class-methods-use-this: "off"*/
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import DocumentForm from '../document/DocumentForm';
+
+class CommonModal extends React.Component {
+  componentDidMount() {
+    $('.modal').modal();
+    $('select').material_select();
+    $('#title').parent().find('label').addClass('active');
+  }
+
+  render() {
+    const { auth } = this.props;
+    return (
+      <div>
+        <div id="modal1" className="modal">
+          <div>
+            <a href="#"
+              className="btn-floating btn-flat blue-grey closeModal modal-close">
+              <i className="material-icons">cancel</i></a>
+          </div>
+          <div className="modal-content">
+            <h4>Document</h4>
+            <DocumentForm auth={auth}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+CommonModal.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+/**
+ * @param  {object} state
+ * @return {object}
+ */
+function mapStateToProps(state) {
+  return {
+    auth: state.auth,
+  };
+}
+
+export default connect(mapStateToProps)(CommonModal);
---
+++ client/components/document/DocumentForm.js
@@ -0,0 +1,247 @@
+import React, { PropTypes } from 'react';
+import toastr from 'toastr';
+import classnames from 'classnames';
+import 'froala-editor/js/froala_editor.pkgd.min';
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'font-awesome/css/font-awesome.css';
+import FroalaEditor from 'react-froala-wysiwyg';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as documentActions from '../../actions/documentActions';
+import { addFlashMessage } from '../../actions/flashMessages';
+
+
+class DocumentForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      document: {},
+      errors: {},
+      select: Object.assign({}, props.docValue).access,
+      documentTitle: Object.assign({}, props.docValue).title,
+      model: Object.assign({}, props.docValue).content,
+      ownerId: Object.assign({}, props.docValue).ownerId,
+      displaySaveButton: true
+    };
+    this.onChange = this.onChange.bind(this);
+    this.updateSelectState = this.updateSelectState.bind(this);
+    this.handleModelChange = this.handleModelChange.bind(this);
+    this.saveDocument = this.saveDocument.bind(this);
+    this.updateDocument = this.updateDocument.bind(this);
+  }
+
+  componentDidMount() {
+    $('#mySelectBox').on('change', this.updateSelectState);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.docValue.id !== nextProps.docValue.id) {
+      // Necessary to populate form when existing documents loaded directly.
+      this.setState({
+        documentTitle: Object.assign({}, nextProps.docValue).title,
+        model: Object.assign({}, nextProps.docValue).content,
+        select: Object.assign({}, nextProps.docValue).access,
+        ownerId: Object.assign({}, nextProps.docValue).ownerId,
+      });
+    }
+    this.setState({ displaySaveButton: true });
+    if (nextProps.docValue.ownerId !== this.props.auth.user.userId) {
+      $('.fr-wrapper').froalaEditor('edit.off');
+      this.setState({ displaySaveButton: false });
+    }
+    if (nextProps.docValue.id === '') {
+      this.setState({ displaySaveButton: true });
+    }
+  }
+
+  onChange(event) {
+    const field = event.target.name;
+    const document = this.state.document;
+    document[field] = event.target.value;
+    document.ownerId = this.props.auth.user.userId;
+    document.role = String(this.props.auth.user.userRoleId);
+    this.setState({ document, documentTitle: event.target.value });
+  }
+
+  updateSelectState(event) {
+    this.setState({ select: event.target.value });
+  }
+
+  handleModelChange(model) {
+    const document = this.state.document;
+    document.content = model;
+    this.setState({ document, model });
+  }
+
+  saveDocument(event) {
+    event.preventDefault();
+    this.props.actions.saveDocument(this.state.document)
+      .then(() => {
+        toastr.success('Document Successfully Saved');
+        $('#modal1').modal('close');
+      })
+      .catch(() => {
+        this.props.addFlashMessage({
+          type: 'error',
+          text: 'Unable to add document, Confirm Document title conflict'
+        });
+        toastr.error(
+          'Unable to save document');
+        $('#modal1').modal('close');
+      });
+  }
+
+  updateDocument(event) {
+    event.preventDefault();
+    this.props.actions.updateDocument(this.state.document)
+      .then(() => {
+        toastr.success('Document Successfully Saved');
+        $('#modal1').modal('close');
+      })
+      .catch(() => {
+        this.props.addFlashMessage({
+          type: 'error',
+          text: 'Unable to update document, Confirm Document title conflict'
+        });
+        toastr.error(
+          'Unable to update document');
+        $('#modal1').modal('close');
+      });
+  }
+
+  redirect() {
+    toastr.success('Document Successfully Saved');
+    this.context.router.push('/document');
+    $('#modal1').modal('close');
+  }
+
+  render() {
+    const isValue = this.props.currentDocument;
+    const { displaySaveButton } = this.state;
+    const form = (
+      <form>
+        <div className="row">
+          {displaySaveButton ? '' : 'View Only'}
+          <div className="input-field col s12">
+            <input
+              icon="subject"
+              id="title"
+              type="text"
+              value={this.state.documentTitle}
+              name="title"
+              placeholder="Enter a Title here!"
+              className="validate"
+              onChange={this.onChange} />
+            <label id="labeltitle"
+              htmlFor="title" className="active">Title
+            </label>
+          </div>
+
+          <div className="input-field col s12">
+            <FroalaEditor
+              tag="textarea"
+              model={this.state.model}
+              onModelChange={this.handleModelChange} />
+          </div>
+
+          <br />
+
+          <div className="input-field col s12">
+            <select name="access" id="mySelectBox"
+              value={this.state.select}
+              className="browser-default" onChange={this.updateSelectState}>
+              <option value="" disabled >Document Visibility Access</option>
+              <option value="public">Public</option>
+              <option value="private">Private</option>
+              <option value="role">Role</option>
+            </select>
+          </div>
+
+          <div className={classnames('input-field col s12', {
+            hide: displaySaveButton === false
+          })}>
+            <input
+              icon="save"
+              id="saveButton"
+              type="submit"
+              value="Save"
+              className="btn waves-effect waves-light blue-grey darken-1"
+              onClick={isValue ? this.updateDocument : this.saveDocument} />
+
+          </div>
+        </div>
+      </form>
+    );
+    return (
+      <div>
+        {form}
+      </div>
+    );
+  }
+}
+
+DocumentForm.propTypes = {
+  auth: PropTypes.object.isRequired,
+  onChange: PropTypes.func,
+  docValue: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired,
+  documents: PropTypes.array.isRequired,
+  currentDocument: PropTypes.string,
+  addFlashMessage: PropTypes.func.isRequired
+};
+
+/**
+ * @param {any} documents
+ * @param {any} id
+ * @returns {any} object
+ */
+function getDocumentById(documents, id) {
+  const document = documents.filter((doc) => {
+    return String(doc.id) === id;
+  });
+  if (document) {
+    return document[0]; // The filter method returns an array
+  }
+  return null;
+}
+
+
+/**
+ * @param {any} state
+ * @returns {any}
+ */
+function mapStateToProps(state) {
+  const currentState = state.handleDocuments;
+  const documentId = currentState.chosenDocument;
+  let document = {
+    id: '',
+    title: '',
+    content: '',
+    access: '',
+    ownerId: ''
+  };
+  if (documentId > 0) {
+    document = getDocumentById(currentState.documents, documentId);
+  }
+  return {
+    documents: currentState.documents,
+    currentDocument: currentState.chosenDocument,
+    docValue: document,
+    auth: state.auth
+  };
+}
+
+/**
+ * @param {any} dispatch
+ * @returns {any}
+ */
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(documentActions, dispatch),
+    addFlashMessage: bindActionCreators(addFlashMessage, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DocumentForm);
