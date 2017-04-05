728f478ab1fe484b79369e1e58be33cb6f0f4ee2
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-04-05 22:39:12 +0100

    feature(document): refactor document controller for fetching documents

---
+++ server/seeders/20170405111234-document.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Document', [{
+      title: 'Enitan',
+      content: 'You is beautiful, you is kind, you is lovely, and even though there be people who tell you that you will amount to nothing, you can count on me to be true and to say the truth. You are brilliant, I say this to you, I say this about you. You have me Enitan, I solemnly swear, you have me.',
+      access: 'public',
+      ownerId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: 'Essays In Love',
+      content: 'Love is the complete absence of self. Unadulterated love that is.',
+      access: 'private',
+      ownerId: 2,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      title: 'Shit hits the fan',
+      content: 'Stuff happen that throw you into limbo and wonder if you are good enough',
+      access: 'role',
+      ownerId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+    ], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
