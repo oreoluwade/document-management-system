8a34a9cb25b5299936fcc833ebcc3326f3ee19c4
Oreoluwa Aboluwarin <oreoluwa.aboluwarin@andela.com>
2017-04-11 05:55:20 +0100

    test(e2e): fix e2e signup test

--- client/tests/e2e/signup.test.js
+++ client/tests/e2e/signup.test.js
@@ -5,7 +5,7 @@
 const config = require('../../../nightwatch.conf.js');
 
 module.exports = {
-  '@disabled': true,
+  // '@disabled': true,
   'Signup Page': function (browser) {
     browser
       .url('http://localhost:7070/signup')
@@ -18,18 +18,15 @@ module.exports = {
     browser
       .url('http://localhost:7070/signup')
       .waitForElementVisible('body')
-      .setValue('input[type=text]', 'lagbaja')
-      .setValue('input[type=text]', 'tamedo')
-      .setValue('input[type=text]', 'lagbaja')
+      .setValue('input[name=userName]', 'lagbaja')
+      .setValue('input[name=firstName]', 'tamedo')
+      .setValue('input[name=lastName]', 'lagbaja')
       .setValue('input[type=email]', 'lagbaja@tamedo.com')
-      .setValue('input[type=password]', 'lagbaja')
-      .setValue('input[type=password]', 'lagbaja')
+      .setValue('input[name=password]', 'lagbaja')
       .click('button[type="submit"]')
-      .waitForElementVisible('h4')
-      .assert.urlEquals('http://localhost:7070/')
-      .assert.containsText('h4', 'DASHBOARD')
-      .assert.containsText('nav', 'Saved Documents')
-      .assert.containsText('nav', 'Logout')
+      .waitForElementVisible('nav', 5000)
+      .assert.urlEquals('http://localhost:7070/signup')
+      .assert.containsText('nav', 'Login')
       .assert.elementNotPresent('#adminTab')
       .assert.cssClassNotPresent('nav', 'admin')
       .end();
