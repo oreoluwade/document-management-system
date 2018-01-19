567b0a1e6b66fa11d8a385f81028591448ec5a38
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-01-19 23:23:16 +0100

    update packages

--- client/index.js
+++ client/index.js
@@ -12,7 +12,6 @@ import setAuthorizationToken from './utils/setAuthorizationToken';
 import '../node_modules/materialize-css/dist/js/materialize.min';
 import '../node_modules/materialize-css/dist/css/materialize.min.css';
 import '../node_modules/material-icons/css/material-icons.css';
-import '../node_modules/sweetalert/dist/sweetalert.css';
 import './styles/styles.css';
 
 const store = configureStore();
