52bec1f21967ccb856ba18fc0caa648b220a117d
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-01-20 14:34:58 +0100

    refactor App component

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import { Header } from './Common';
+import React from 'react';
+import {
+  Route,
+  Switch,
+  BrowserRouter as Router
+} from 'react-router-dom';
 import FlashMessagesList from './FlashMessagesList';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
@@ -15,60 +17,51 @@ import ProfilePage from './ProfilePage';
 import requireAuth from './Utils/RequireAuth';
 import requireAdminAuth from './Utils/RequireAdminAuth';
 
-class App extends Component {
-  render() {
-    // const { location } = this.props;
-    return (
-      <Router>
-        <div>
-          <FlashMessagesList />
-          <Switch>
-            <Route
-              component={HomePage}
-              exact
-              path="/"
-            />
-            <Route
-              path="dashboard"
-              component={requireAuth(DashboardPage)}
-            />
-            <Route
-              path="login"
-              component={LoginPage}
-            />
-            <Route
-              path="signup"
-              component={SignupPage}
-            />
-            <Route
-              path="documents"
-              component={requireAuth(DocumentPage)}
-            />
-            <Route
-              path="profilepage"
-              component={requireAuth(ProfilePage)}
-            />
-            <Route
-              path="about"
-              component={AboutPage}
-            />
-            <Route
-              path="admin/manageroles"
-              component={requireAdminAuth(ManangeRolePage)}
-            />
-            <Route
-              path="admin/handleusers"
-              component={requireAdminAuth(HandleUsersPage)}
-            />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
-}
-
-App.propTypes = {
-  location: PropTypes.object
-};
+const App = () => (
+  <Router>
+    <div>
+      <FlashMessagesList />
+      <Switch>
+        <Route
+          component={HomePage}
+          exact
+          path="/"
+        />
+        <Route
+          path="dashboard"
+          component={requireAuth(DashboardPage)}
+        />
+        <Route
+          path="login"
+          component={LoginPage}
+        />
+        <Route
+          path="signup"
+          component={SignupPage}
+        />
+        <Route
+          path="documents"
+          component={requireAuth(DocumentPage)}
+        />
+        <Route
+          path="profilepage"
+          component={requireAuth(ProfilePage)}
+        />
+        <Route
+          path="about"
+          component={AboutPage}
+        />
+        <Route
+          path="admin/manageroles"
+          component={requireAdminAuth(ManangeRolePage)}
+        />
+        <Route
+          path="admin/handleusers"
+          component={requireAdminAuth(HandleUsersPage)}
+        />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
