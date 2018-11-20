a6c7205d51168eedc5de25628dc99d9c06e5b5e3
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-20 12:04:22 +0100

    refactor AboutPage component

--- client/components/AboutPage/index.jsx
+++ client/components/AboutPage/index.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
 
-class AboutPage extends React.Component {
-  render() {
-    return (
-      <div className="card-panel z-depth-8" id="aboutcard">
-        <ul>
-          <h5> - Create, edit, and delete documents</h5>
-          <h5> - Restrict access to your documents</h5>
-          <h5> - Access Documents of persons in the same role as you</h5>
-        </ul>
-      </div>
-    );
-  }
-}
+const AboutPage = () => (
+  <div className="card-panel z-depth-8" id="aboutcard">
+    <h1>FEATURES</h1>
+    <h5> - Create, edit, and delete documents for a variety of purposes</h5>
+    <h5> - Restrict access to your documents</h5>
+    <h5> - Utilize your <strong>read access</strong> to documents of persons with your role.</h5>
+  </div>
+);
 
 export default AboutPage;
