b95ccbc06f21900e3161de326b32d9a9e145d364
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-20 22:10:06 +0100

    refactor reducers

--- client/reducers/auth.js
+++ client/reducers/auth.js
@@ -3,12 +3,17 @@ import { SET_CURRENT_USER } from '../actions/actionTypes';
 import initialState from './initialState';
 
 export default (state = initialState, action = {}) => {
+  let returnValue;
   switch (action.type) {
     case SET_CURRENT_USER:
-      return {
+      returnValue = {
         isAuthenticated: !isEmpty(action.user),
         user: action.user
       };
-    default: return state;
+      break;
+    default:
+      returnValue = state;
+      break;
   }
+  return returnValue;
 };
--- client/reducers/roleReducer.js
+++ client/reducers/roleReducer.js
@@ -1,12 +1,7 @@
 import * as types from '../actions/actionTypes';
 import initialState from './initialState';
 
-/**
- * [roleReducer description]
- * @param  {object} [state=initialState.manageRoles] initial state
- * @param  {object} action
- * @return {object}
- */
+
 export default function roleReducer(state = initialState.manageRoles, action) {
   switch (action.type) {
     case types.LOAD_ROLE_SUCCESS:
