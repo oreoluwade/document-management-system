87f0d3eb7a5b9e0cd18d3f00b9e871215caf6bce
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-20 19:48:30 +0100

    refactor HomePage to functional component

--- client/components/HomePage/index.jsx
+++ client/components/HomePage/index.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class HomePage extends React.Component {
-  render() {
-    return (
-      <div className="card-panel z-depth-8" id="homecard">
-        <h3>DOCUMENT MANAGEMENT SYSTEM</h3>
-        <p>Create, Edit and Delete Documents for all purposes</p>
-        <Link
-          to="about"
-          className="btn waves-effect waves-light blue-grey">Learn More</Link>
-      </div>
-    );
-  }
-}
+const HomePage = () => (
+  <div className="card-panel z-depth-8" id="homecard">
+    <h3>DOCUMENT MANAGEMENT SYSTEM</h3>
+    <p>Create, Edit and Delete Documents for all purposes</p>
+    <Link
+      to="about"
+      className="btn waves-effect waves-light blue-grey"
+    >
+      Learn More
+    </Link>
+  </div>
+);
 
 export default HomePage;
