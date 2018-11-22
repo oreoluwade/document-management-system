d2b0a0e7fb852e3638d7c807f79ade6ee1eeab65
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-22 13:35:33 +0100

    fix role api failing tests

--- server/controllers/role.js
+++ server/controllers/role.js
@@ -5,17 +5,18 @@ const { Role } = models;
 export default {
   createRole(req, res) {
     const { title } = req.body;
-    const bodyIncludesTitle = Object.keys(req.body).includes('title');
-    if (!!title.trim() || !bodyIncludesTitle) return res.send({ message: 'You must supply a valid title' });
+    const bodyDoesNotIncludeTitle = !Object.keys(req.body).includes('title');
+    const titleIsAnEmptyString = title && !title.trim();
+    if (bodyDoesNotIncludeTitle || titleIsAnEmptyString) return res.status(400).send({ message: 'You must supply a valid title' });
     Role.findOne({ where: { title } })
       .then((roleExists) => {
         if (roleExists) {
-          return res.status(409).json({ message: 'Role Already Exists!' });
+          return res.status(409).send({ message: 'Role Already Exists!' });
         }
         Role.create({ title })
           .then((role) => {
             const responseObject = { ...role.dataValues, message: 'Role Successfully Created!' };
-            return res.send(responseObject);
+            return res.status(201).send(responseObject);
           })
           .catch(error => res.send(error));
       })
@@ -23,19 +24,31 @@ export default {
   },
 
   updateRole(req, res) {
-    Role.findById(req.params.id)
+    Role.findByPk(req.params.id)
       .then((role) => {
-        if (!role) return res.status(404).send({ message: `No role with id: ${req.params.role}` });
-        role.update(req.body.title)
-          .then(updatedRole => res.status(200)
-          .send(updatedRole));
+        if (!role) return res.status(404).send({ message: `No role with id: ${req.params.id}` });
+        Role.findAll()
+          .then((foundRoles) => {
+            const roleTitles = foundRoles.map(item => item.title);
+            if (roleTitles.includes(req.body.title)) {
+              if (role.title === req.body.title) {
+                return res.send(role);
+              }
+              return res.status(409).send({ message: 'Role already exists' });
+            }
+            role.update({ title: req.body.title })
+              .then(updatedRole => res.status(200).send(updatedRole));
+          });
       });
   },
 
   deleteRole(req, res) {
-    Role.findById(req.params.id)
+    Role.findByPk(req.params.id)
       .then((role) => {
-        if (!role) return res.status(404).send({ message: `No role with id: ${req.params.role}` });
+        if (!role) return res.status(404).send({ message: `No role with id: ${req.params.id}` });
+        if (Number(req.params.id) === req.decoded.id) {
+          return res.status().send({ message: 'You cannot delete admin' });
+        }
         role.destroy()
           .then(() => res.send({ message: 'Role Successfully Deleted' }))
           .catch(error => res.send({ error }));
@@ -44,9 +57,9 @@ export default {
   },
 
   getRole(req, res) {
-    Role.findById(req.params.id)
+    Role.findByPk(req.params.id)
       .then((role) => {
-        if (!role) return res.status(404).send({ message: `No role with id: ${req.params.role}` });
+        if (!role) return res.status(404).send({ message: `No role with id: ${req.params.id}` });
         return res.send(role);
       });
   },
--- server/test/server/role.spec.js
+++ server/test/server/role.spec.js
@@ -3,171 +3,202 @@ import app from '../../app';
 import models from '../../models';
 import resourceCreator from '../resourceCreator';
 
-
 const request = require('supertest')(app);
 
-const Role = models.Role;
+const { Role } = models;
 
 const adminParams = resourceCreator.createAdmin();
 const adminRoleParams = resourceCreator.createAdminRole();
 const regularRoleParams = resourceCreator.createRegularRole();
 
-describe('Role API', () => {
+describe('THE ROLE API', () => {
   let token;
-  let role;
 
   before((done) => {
-    Role.create(adminRoleParams)
-      .then((adminRole) => {
-        adminParams.roleId = adminRole.id;
-        request.post('/user')
-          .send(adminParams)
-          .end((error, response) => {
-            token = response.body.token;
-            expect(response.status).to.equal(201);
-            done();
+    models.sequelize.sync({ force: true })
+      .then(() => {
+        Role.create(adminRoleParams)
+          .then(() => {
+            request.post('/user/register')
+              .send(adminParams)
+              .then((response) => {
+                token = response.body.token;
+                expect(response.status).to.equal(201);
+                done();
+              });
           });
       });
   });
 
-  beforeEach((done) => {
-    Role.create(regularRoleParams)
-      .then((regularRole) => {
-        role = regularRole;
-        done();
-      });
-  });
-
-  afterEach(() => Role.destroy({ where: { id: role.id } }));
-
-  after(() => models.sequelize.sync({ force: true }));
-
   describe('Role Requests', () => {
-    describe('Create Role', () => {
-      it(`should create a role when the required field a value
-      is valid`, (done) => {
+    describe('POST REQUEST: /role', () => {
+      it('Should create a role when required params are provided', (done) => {
         const newRole = { title: 'almighty admin' };
         request.post('/role')
           .set({ Authorization: token })
           .send(newRole)
-          .end((error, response) => {
+          .then((response) => {
             expect(response.status).to.equal(201);
-            expect(response.body.role.title).to.equal(newRole.title);
+            expect(response.body.title).to.equal(newRole.title);
             done();
           });
       });
 
-      it('should not create a role when the required field is invalid',
-        (done) => {
-          const newRole = { name: 'guest' };
-          request.post('/role')
-            .set({ Authorization: token })
-            .send(newRole)
-            .expect(400);
-          done();
-        });
-
-      it('should not create a role if the role already exists', (done) => {
+      it('should not create a role when title is not supplied', (done) => {
+        const newRole = { name: 'guest' };
         request.post('/role')
           .set({ Authorization: token })
-          .send(regularRoleParams)
-          .expect(409);
-        done();
+          .send(newRole)
+          .expect(400)
+          .then((response) => {
+            expect(response.body.message).to.equal('You must supply a valid title');
+            done();
+          });
+      });
+
+      it('should not create a role if the role already exists', (done) => {
+        Role.create(regularRoleParams)
+          .then(() => {
+            request.post('/role')
+              .set({ Authorization: token })
+              .send(regularRoleParams)
+              .expect(409)
+              .then((response) => {
+                expect(response.body.message).to.equal('Role Already Exists!');
+                done();
+              });
+          });
       });
     });
 
-    describe('GET roles', () => {
+    describe('GET REQUEST: /role', () => {
       it('should not return roles when no token is provided', (done) => {
-        request.get('/role')
-          .expect(401);
-        done();
+        request
+          .get('/role')
+          .expect(401)
+          .then((response) => {
+            expect(response.body.message).to.equal('No token provided!');
+            done();
+          });
       });
 
-      it('should not return roles when the token provided is invalid',
-        (done) => {
-          request.get('/role')
-            .set({ Authorization: 'invalidToken' })
-            .expect(401, done);
-        });
+      it('should not return roles when the token provided is invalid', (done) => {
+        request.get('/role')
+          .set({ Authorization: 'invalidToken' })
+          .expect(401)
+          .then((response) => {
+            expect(response.body.message).to.equal('Invalid token');
+            done();
+          });
+      });
 
-      it(`should return the roles requested for when the requester's
-      token is valid`,
-        (done) => {
-          request.get('/role')
-            .set({ Authorization: token })
-            .end((error, response) => {
-              expect(response.status).to.equal(200);
-              // eslint-disable-next-line no-unused-expressions
-              expect(Array.isArray(response.body)).to.be.true;
-              expect(response.body.length).to.be.greaterThan(0);
-              done();
-            });
-        });
-    });
-
-    describe('GET role by ID', () => {
-      it('should not return any role when the ID supplied is invalid',
-        (done) => {
-          request.get('/role/999999')
-            .set({ Authorization: token })
-            .end((error, response) => {
-              expect(response.status).to.equal(404);
-              done();
-            });
-        });
-
-      it('should return the role requested when a valid ID is provided',
-        (done) => {
-        request.get(`/role/${role.id}`)
+      it('should return the roles when the requester\'s token is valid', (done) => {
+        request.get('/role')
           .set({ Authorization: token })
-          .end((error, response) => {
+          .then((response) => {
             expect(response.status).to.equal(200);
+            // eslint-disable-next-line no-unused-expressions
+            expect(Array.isArray(response.body)).to.be.true;
+            expect(response.body.length).to.be.greaterThan(0);
+            expect(response.body[0].title).to.equal('admin');
             done();
           });
       });
     });
 
-    describe('Update Role (/PUT)', () => {
+    describe('GET REQUEST: /role/:id', () => {
+      it('should not return any role when the ID supplied is invalid', (done) => {
+        request.get('/role/999999')
+          .set({ Authorization: token })
+          .then((response) => {
+            expect(response.status).to.equal(404);
+            expect(response.body.message).to.equal('No role with id: 999999');
+            done();
+          });
+      });
+
+      it('should return the role requested when a valid ID is provided', (done) => {
+        request.get('/role/1')
+          .set({ Authorization: token })
+          .then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body.title).to.equal('admin');
+            done();
+          });
+      });
+    });
+
+    describe('PUT REQUEST /role/:id', () => {
       it('should not update the role if a wrong ID is supplied', (done) => {
         const fieldsToUpdate = { title: 'occasional user' };
         request.put('/role/999999')
           .set({ Authorization: token })
           .send(fieldsToUpdate)
-          .expect(404, done);
+          .then((response) => {
+            expect(response.status).to.equal(404);
+            expect(response.body.message).to.equal('No role with id: 999999');
+            done();
+          });
       });
 
-      it('should allow update the role when the ID is valid', (done) => {
+      it('should allow update of the role when the ID is valid', (done) => {
         const fieldsToUpdate = { title: 'observer' };
-        request.put(`/role/${role.id}`)
+        request.put('/role/2')
           .set({ Authorization: token })
           .send(fieldsToUpdate)
-          .end((error, response) => {
+          .then((response) => {
             expect(response.status).to.equal(200);
             expect(response.body.title).to.equal(fieldsToUpdate.title);
             done();
           });
       });
+
+      it('Do nothing if the title remains the same for the same role', (done) => {
+        const fieldsToUpdate = { title: 'observer' };
+        request.put('/role/2')
+          .set({ Authorization: token })
+          .send(fieldsToUpdate)
+          .then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body.title).to.equal('observer');
+            done();
+          });
+      });
+
+      it('should prevent update when a role with a similar name already exists', (done) => {
+        const fieldsToUpdate = { title: 'observer' };
+        request.put('/role/3')
+          .set({ Authorization: token })
+          .send(fieldsToUpdate)
+          .then((response) => {
+            expect(response.status).to.equal(409);
+            expect(response.body.message).to.equal('Role already exists');
+            done();
+          });
+      });
     });
 
-    describe('Deleting a ROLE', () => {
-      it('should not delete a role when an invalid roleId is supplied',
-        (done) => {
-          request.delete('/role/999999')
-            .set({ Authorization: token })
-            .expect(404, done);
-        });
+    describe('DELETE REQUEST: /role:id', () => {
+      it('should not delete a role when an invalid roleId is supplied', (done) => {
+        request.delete('/role/999999')
+          .set({ Authorization: token })
+          .expect(404)
+          .then((response) => {
+            expect(response.status).to.equal(404);
+            expect(response.body.message).to.equal('No role with id: 999999');
+            done();
+          });
+      });
 
-      it('should delete a role when valid ID is provided',
-        (done) => {
-          request.delete(`/role/${role.id}`)
-            .set({ Authorization: token })
-            .end((error, response) => {
-              expect(response.status).to.equal(200);
-              expect(response.body.message)
-                .to.equal('Role Successfully Deleted');
-              done();
-            });
-        });
+      it('should delete a role when valid ID is provided', (done) => {
+        request.delete('/role/3')
+          .set({ Authorization: token })
+          .then((response) => {
+            expect(response.status).to.equal(200);
+            expect(response.body.message).to.equal('Role Successfully Deleted');
+            done();
+          });
+      });
     });
   });
 });
