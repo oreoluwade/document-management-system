197597953b80a4b0572de950619539ce0e9aa07e
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-11-22 07:07:45 +0100

    fix role model tests

--- server/test/models/role.spec.js
+++ server/test/models/role.spec.js
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-expressions */
-
 import { expect } from 'chai';
+import Sequelize from 'sequelize';
 import resourceCreator from '../resourceCreator';
 import model from '../../models';
 
@@ -8,7 +7,7 @@ const { Role } = model;
 
 const adminRole = resourceCreator.createAdminRole();
 
-describe.only('THE ROLE MODEL TEST SUITE', () => {
+describe('THE ROLE MODEL TEST SUITE', () => {
   before(() => model.sequelize.sync({ force: true }));
   describe('Creating a new role', () => {
     let role;
@@ -27,12 +26,12 @@ describe.only('THE ROLE MODEL TEST SUITE', () => {
       expect(typeof role).to.equal('object');
     });
 
-    it('should allow a creator define the title of the role created', () => {
-      expect(role).to.include.keys('title');
-      expect(role).to.include.keys('createdAt');
-      expect(role).to.include.keys('updatedAt');
-      expect(role).to.include.keys('id');
-      expect(role.title).to.equal(adminRole.title);
+    it('should allow creation of a role once title is defined', () => {
+      expect(role.dataValues).to.include.keys('title');
+      expect(role.dataValues).to.include.keys('createdAt');
+      expect(role.dataValues).to.include.keys('updatedAt');
+      expect(role.dataValues).to.include.keys('id');
+      expect(role.dataValues.title).to.equal(adminRole.title);
     });
   });
 
@@ -40,40 +39,26 @@ describe.only('THE ROLE MODEL TEST SUITE', () => {
     after(() => model.sequelize.sync({ force: true }));
 
     describe('Validation for the title field', () => {
-      it('should ensure that a title is given before a role can be created',
-        (done) => {
-          Role.create()
-            .then((result) => {
-              console.log('VALIDATION', result)
-              // expect(result.message).to.equal('You must supply a valid title');
-            })
-            .catch((error) => {
-              console.log('=========>', error);
+      it('should ensure that title cannot be null', (done) => {
+        Role.create()
+          .catch((error) => {
+            expect(error instanceof Sequelize.ValidationError).to.be.true;
+            expect(error.errors[0].message).to.equal('Role.title cannot be null');
+            done()
+          });
+      });
 
-              // expect(error.SequelizeValidationError).to.equal('title cannot be null');
-              // done();
-              done()
-            });
-        });
-
-      it(`should ensure that it is impossible
-      to create two roles with the same title`,
-        (done) => {
-          Role.create(adminRole)
-            .then(() => {
-              // attempting to create a second role with the same title as the first
-              Role.create(adminRole)
-                .then((result) => {
-                  console.log('Duplicate Roles', result);
-                  expect(result.message).to.equal('Role Already Exists!')
-                })
-                // .catch((error) => {
-                //   expect(/UniqueConstraintError/.test(error.name)).to.be.true;
-                //   done();
-                // });
-            });
-            done();
-        });
+      it('should prevent the creation of two roles with the same title', (done) => {
+        Role.create(adminRole)
+          .then(() => {
+            Role.create(adminRole)
+              .catch(err => {
+                expect(err instanceof Sequelize.UniqueConstraintError).to.be.true;
+                expect(err.errors[0].message).to.equal('title must be unique');
+                done();
+              });
+          })
+      });
     });
   });
 });
