912ec60d75b95149cb00fca98ee236c1c46592c1
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2019-12-07 13:51:29 +0000

    Display more stuff on document card

--- client/components/document/document-card.jsx
+++ client/components/document/document-card.jsx
@@ -3,42 +3,57 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
+import MenuBookIcon from '@material-ui/icons/MenuBookSharp';
+
+import { formatRelative } from 'date-fns';
 
 const useStyles = makeStyles({
   card: {
     minWidth: '80%',
     backgroundColor: '#CDCDCD',
-    marginBottom: '0.4rem'
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)'
+    marginBottom: '0.4rem',
+    display: 'flex',
+    flexDirection: 'column'
   },
   title: {
-    fontSize: 14
+    fontSize: 14,
+    fontWeight: 'bold'
   },
-  pos: {
-    marginBottom: 12
+  date: {
+    color: 'blue',
+    fontSize: 10,
+    fontStyle: 'italic'
+  },
+  textContent: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  action: {
+    display: 'flex',
+    justifyContent: 'flex-end'
   }
 });
 
 const DocumentCard = ({ document }) => {
   const classes = useStyles();
 
+  const getDate = document => {
+    return formatRelative(new Date(document.createdAt), new Date());
+  };
+
   return (
     <Card className={classes.card}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary">
-          {document.title}
-        </Typography>
+      <CardContent className={classes.textContent}>
+        <Typography className={classes.title}>{document.title}</Typography>
+        <Typography className={classes.date}>{`Created ${getDate(
+          document
+        )}`}</Typography>
       </CardContent>
-      <CardActions>
+      <CardActions className={classes.action}>
         <Link to={`/render-document/${document.id}`} className="navbar-link">
-          <Button size="small">View Document</Button>
+          <MenuBookIcon />
         </Link>
       </CardActions>
     </Card>
