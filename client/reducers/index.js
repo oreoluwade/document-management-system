35833ae359899d68c8c9dbf0b5b97819a1e1ce65
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2019-12-12 05:21:32 +0000

    Remove unused reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,4 @@
 import { combineReducers } from 'redux';
-// import { reducer } from 'react-redux-sweetalert';
-// import handleUsers from './userReducer';
-// import authUser from './documentReducer';
-// import manageRoles from './roleReducer';
 import auth from './auth';
 import user from './user';
 import admin from './admin';
