22a26d72f67a96a4915f1b81fb74591e0492d2fd
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2019-12-15 09:58:16 +0000

    Deprecate default export in entry point

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -11,32 +11,32 @@ const port = process.env.PORT || 7070;
 /* eslint-disable no-console */
 const env = (process.env.NODE_ENV || 'development') === 'development';
 if (env) {
-    const compiler = webpack(config);
-    app.use(
-        require('webpack-dev-middleware')(compiler, {
-            noInfo: true,
-            publicPath: config.output.publicPath
-        })
-    );
+  const compiler = webpack(config);
+  app.use(
+    require('webpack-dev-middleware')(compiler, {
+      noInfo: true,
+      publicPath: config.output.publicPath
+    })
+  );
 
-    app.use(require('webpack-hot-middleware')(compiler));
+  app.use(require('webpack-hot-middleware')(compiler));
 } else {
-    app.use(express.static('dist'));
-    app.use(compression());
+  app.use(express.static('dist'));
+  app.use(compression());
 }
 
 app.get('*', (request, response) => {
-    response.sendFile(
-        path.join(__dirname, `../${env ? 'client' : 'dist'}/index.html`)
-    );
+  response.sendFile(
+    path.join(__dirname, `../${env ? 'client' : 'dist'}/index.html`)
+  );
 });
 
 app.listen(port, error => {
-    if (error) {
-        console.log(error);
-    }
-    if (env) open(`http://localhost:${port}`);
-    console.log(`Server running on port ${port}`);
+  if (error) {
+    console.log(error);
+  }
+  if (env) open(`http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
 
-export default app;
+module.exports = app;
