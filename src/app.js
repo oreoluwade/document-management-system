1460f3e9b4e4af3f5545962de367fda2c4f720cf
oreoluwade <oreoluwade@gmail.com>
2019-12-16 12:00:28 +0000

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import logger from 'morgan';
+import cors from 'cors';
 import { roleRoutes, userRoutes, documentRoutes } from './routes';
 
 dotenv.config();
@@ -12,9 +13,30 @@ const app = express();
 
 app.set('hiddenDetails', secret);
 
+// CORS RULES
+const whitelist = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
+    'https://docol.herokuapp.com'
+];
+
+const corsOptions = {
+    origin: function(origin, callback) {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            const error = new Error('Not allowed by CORS');
+            error.status = 401;
+            callback(error);
+        }
+    }
+};
+
 // Parse incoming requests data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 
 // Requiring the routes into the application
